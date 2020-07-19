f185a207fa00348dd8d25144cfebbf062172b4dd
Irene Argeiti <supernova1788@hotmail.com>
2020-07-19 18:23:03 +0300

    added webconfig home controller and renamed main

---
+++ src/main/java/com/groupproject/tomeraiders/TomeRaidersApplication.java
@@ -0,0 +1,13 @@
+package com.groupproject.tomeraiders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomeRaidersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomeRaidersApplication.class, args);
+	}
+
+}
