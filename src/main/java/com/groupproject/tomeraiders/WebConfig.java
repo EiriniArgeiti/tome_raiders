a5b907cdfbd4ee6c82512b1d2b0cb31f40718c59
Irene Argeiti <supernova1788@hotmail.com>
2020-09-06 18:30:11 +0300

    fixed absolute path for images

--- src/main/java/com/groupproject/tomeraiders/WebConfig.java
+++ src/main/java/com/groupproject/tomeraiders/WebConfig.java
@@ -6,11 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/media/**")
-                .addResourceLocations("file:/C:/Users/super/Documents/tome_raiders/src/main/resources/static/media/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/media/");
     }
-
 }
